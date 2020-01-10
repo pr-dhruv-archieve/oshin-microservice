e1ebcfc165963a8bfd86a0bc97d61056df62bbf5
unknown <comviva@MCKD-04039.COMVIVA.COM>
2020-01-10 15:54:11 +0530

    Project Initiated

---
+++ movie-info-service/src/test/java/com/comviva/movie/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comviva.movie.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
